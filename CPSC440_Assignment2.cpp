5768cb64641da82696eb973daa0e12a28be06f96
adantuon <adantuon@mail.umw.edu>
2025-05-31 19:20:03 -0400

    Set up basic game loop

--- CPSC440_Assignment2.cpp
+++ CPSC440_Assignment2.cpp
@@ -3,7 +3,51 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <stdio.h>
 
 int main() {
+	int width = 640;
+	int height = 480;
+	bool exit = false;
 
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *eventQueue = NULL;
+
+	if (!al_init()) {
+		return(-1);
+	}
+
+	display = al_create_display(width, height + 40);
+
+	if (!display) {
+		return(-1);
+	}
+
+	al_install_mouse();
+	al_init_primitives_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *font = al_load_font("PressStart2P.ttf", 16, 0);
+	if (font == NULL) {
+		printf("Font is NULL\n");
+		return(-1);
+	}
+
+	eventQueue = al_create_event_queue();
+	al_register_event_source(eventQueue, al_get_display_event_source(display));
+	al_register_event_source(eventQueue, al_get_mouse_event_source());
+
+	while (!exit) {
+		ALLEGRO_EVENT event;
+		al_wait_for_event(eventQueue, &event);
+
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			exit = true;
+		}
+
+	}
+
+	al_destroy_event_queue(eventQueue);
+	al_destroy_display(display);
 }
